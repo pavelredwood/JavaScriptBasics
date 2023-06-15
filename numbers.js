73baa16a7425f8f697d5fcfaf92ff3b47b36a8c8
Pawel Sapron <pavel.redwood@gmail.com>
2023-06-16 01:11:09 +0300

    Add more practice with numbers

--- numbers.js
+++ numbers.js
@@ -15,3 +15,13 @@ let b = 2 ** 3; // b = 8
 
 // Modulo
 let c = 10 % 3; // c = 1
+
+
+let a2 = 10
+let b2 = 7 * a
+console.log(b)
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
