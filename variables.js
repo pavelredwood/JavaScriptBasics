28f15c734d31ece7dc2761c1ed3ae60bac28c590
Pawel Sapron <pavel.redwood@gmail.com>
2023-06-15 21:21:36 +0300

    Add basic files and fix first task for variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+/* Task 1
+Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output “John”).  */
+
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
+
+/* solution:
+let admin, name; // can declare two variables at once
+name = "John";
+admin = name;
+alert( admin ); // "John" */
