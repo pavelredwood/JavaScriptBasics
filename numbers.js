2266ad30d2860f451fe1ef560bd6e4c0dfaf06d7
Pawel Sapron <pavel.redwood@gmail.com>
2023-06-15 21:49:34 +0300

    Add basic math file

---
+++ numbers.js
@@ -0,0 +1,17 @@
+// Addition
+let x = 1 + 2; // x = 3
+
+// Subtraction
+let y = 5 - 2; // y = 3
+
+// Multiplication
+let z = 3 * 4; // z = 12
+
+// Division
+let a = 12 / 4; // a = 3
+
+// Exponentiation
+let b = 2 ** 3; // b = 8
+
+// Modulo
+let c = 10 % 3; // c = 1
