7ae68d22262692bbb20d96f544d2aeb60e0ff249
Pawel Sapron <pavel.redwood@gmail.com>
2023-07-06 22:56:24 +0300

    Add short return function

--- return.js
+++ return.js
@@ -2,3 +2,12 @@ function actualIphone () {
     phoneName = "iPhone 14 Pro Max";
     return phoneName;
 }
+
+function random (number) {
+    const result = Math.floor(Math.random() * number);
+    return result;
+}
+
+function randomShort(number) {
+    return Math.floor(Math.random() * number);
+  }
